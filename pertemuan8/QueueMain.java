2b7335c7798043f1d4f4e5e326806d40fca96c7a
AdrianZidan <124847213+Xenathra@users.noreply.github.com>
2024-04-25 17:07:30 +0700

    Add files via upload
    tugas pertemuan 8 queue

---
+++ pertemuan8/QueueMain.java
@@ -0,0 +1,29 @@
+package pertemuan8;
+
+public class QueueMain {
+
+	public static void main(String[] args) {
+        StrukturQueue queue = new StrukturQueue();
+      
+
+     // Before enqueue 4x
+        System.out.println("### Before enqueue 4x ###");
+        System.out.println("size: " + queue.size());
+        System.out.println("isEmpty: " + queue.isEmpty());
+        System.out.print("element queue: ");
+        queue.displayElements();
+        System.out.println();
+
+        // Enqueue 4x
+        System.out.println("### Enqueue 4x ###");
+        queue.enqueue(2);
+        queue.enqueue(7);
+        queue.enqueue(6);
+        queue.enqueue(1);
+        System.out.println("size: " + queue.size());
+        System.out.println("isEmpty: " + queue.isEmpty());
+        System.out.print("element queue: ");
+        queue.displayElements();
+        System.out.println("front: " + queue.front());
+	}
+}
